fa74ca94dc1e6fb48489d716b6ccfe7f8582b301
pkmango <33579530+pkmango@users.noreply.github.com>
2023-03-13 23:35:18 +0300

    Customizing Enemy Behavior

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,17 +3,19 @@
 public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
+    public float playerDetectionDistance = 10f;
+    public LayerMask playerLayerMask;
 
     private GameObject player;
     private Vector3 moveDirection;
 
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        Vector3 playerPosition = GetPlayerPosition();
 
         // Altitude correction
-        transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
-        moveDirection = Vector3.Normalize(player.transform.position - transform.position);
+        transform.position = new Vector3(transform.position.x, playerPosition.y, transform.position.z);
+        moveDirection = Vector3.Normalize(playerPosition - transform.position);
     }
 
     void Update()
@@ -21,8 +23,19 @@ public class Enemy : MonoBehaviour
         transform.Translate(speed * Time.deltaTime * moveDirection, Space.World);
     }
 
-    private void OnDestroy()
+    private Vector3 GetPlayerPosition()
     {
-        //Debug.Log(gameObject.name + " has been destroyed");
+        Collider[] playerCollider = new Collider[1];
+        Physics.OverlapSphereNonAlloc(transform.position, playerDetectionDistance, playerCollider, playerLayerMask);
+        if (playerCollider[0] != null)
+        {
+            player = playerCollider[0].gameObject;
+            return player.transform.position;
+        }
+        else
+        {
+            Debug.Log("Player not found");
+            return Vector3.zero;
+        }
     }
 }
